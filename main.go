3730b44c44f771d6db253ef84a1366552d65f10f
Matias De Stefano <matias.destefano@mercadolibre.com>
2021-12-23 12:25:41 -0300

    Practica terminada

--- main.go
+++ main.go
@@ -5,35 +5,43 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type producto struct {
-	Id              string  `json:"id"`
-	Nombre          string  `json:"nombre"`
-	Color           string  `json:"color"`
-	Precio          float64 `json:"precio"`
-	Stock           int     `json:"stock"`
-	Codigo          string  `json:"codigo"`
-	Publicado       bool    `json:"publicado"`
-	FechaDeCreacion string  `json:"fechaDeCreacion"`
+	Id              int     `json:"id"`
+	Nombre          string  `json:"nombre" validate:"required"`
+	Color           string  `json:"color" validate:"required"`
+	Precio          float64 `json:"precio" validate:"required"`
+	Stock           int     `json:"stock" validate:"required"`
+	Codigo          string  `json:"codigo" validate:"required"`
+	Publicado       bool    `json:"publicado" validate:"required"`
+	FechaDeCreacion string  `json:"fechaDeCreacion" validate:"required"`
 }
 
+var productos []producto
+
 func main() {
+
+	productos = obtenerProductos()
 	router := gin.Default()
 
 	router.GET("/user/:name", ShowName)
 
-	router.GET("/productos", GetAll)
-	router.GET("/productos/:id", handlerBuscarProductoID)
-	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	productosRouter := router.Group("/productos")
+	productosRouter.GET("/", GetAll)
+	productosRouter.GET("/:id", handlerBuscarProductoID)
+	productosRouter.POST("/", Guardar())
+
+	router.Run() // localhost:8080
 }
 
 func GetAll(c *gin.Context) {
-	productos := obtenerProductos()
-	prodID := c.Query("id")
-	if prodID != "" {
+	prodID, _ := strconv.Atoi(c.Query("id"))
+	if prodID != 0 {
 		for _, p := range productos {
 			if p.Id == prodID {
 				c.JSON(http.StatusOK, gin.H{
@@ -58,8 +66,8 @@ func ShowName(c *gin.Context) {
 func handlerBuscarProductoID(context *gin.Context) {
 	var existe bool
 	productos := obtenerProductos()
-	idProd := context.Param("id")
-	if idProd != "" {
+	idProd, _ := strconv.Atoi(context.Param("id"))
+	if idProd != 0 {
 		for _, p := range productos {
 			if p.Id == idProd {
 				context.JSON(http.StatusOK, gin.H{
@@ -89,4 +97,48 @@ func obtenerProductos() []producto {
 	}
 
 	return productos
+}
+
+func Guardar() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		//VALIDAR TOKEN
+		token := ctx.GetHeader("token")
+		if token != "123456" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": "token invalido",
+			})
+			return
+		}
+
+		var req producto
+		err := ctx.ShouldBindJSON(&req)
+		if err != nil {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		v := validator.New()
+		err = v.Struct(req)
+		if err != nil {
+			for _, err := range err.(validator.ValidationErrors) {
+				mensaje := "el campo " + err.Field() + " es requerido"
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"error": mensaje,
+				})
+				return
+			}
+		}
+
+		lastID := getLastProductID()
+		req.Id = lastID + 1
+		//TODO: Deberia agregar dicho producto a products.json
+		productos = append(productos, req)
+	}
+}
+
+func getLastProductID() int {
+	return productos[len(productos)-1].Id
 }
